7450fc465447cf10ec7f3c2755ce6af0e4d94d0b
dimmih <dimmihzr@gmail.com>
2019-11-09 13:59:29 +0100

    added create and edit for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,48 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: :destroy
   
   def index
-    @questions = Question.all
+    @questions = Question.order("id DESC")
+  end
+
+  def new
+    @question = Question.new
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def create
+    @question = Question.new(quesiton_params)
+    if @question.save
+      flash[:success] = 'New question added.'
+    else
+      flash[:error] = 'There was something wrong!'
+    end
+
+    redirect_to questions_path
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(quesiton_params)
+      flash[:success] = 'Question updated.'
+    else
+      flash[:error] = 'There was something wrong!'
+    end
+
+    redirect_to questions_path
   end
 
   def destroy
@@ -19,5 +60,9 @@ class QuestionsController < ApplicationController
 
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def quesiton_params
+    params.require(:question).permit(:value, :difficulty, :kind, :field_id)
   end
 end
